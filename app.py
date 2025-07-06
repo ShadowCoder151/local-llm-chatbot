d22b2cfda9d3ed26ac24efb310416e7ccadf01a9
ShadowCoder151 <shadowcoder023@gmail.com>
2025-07-06 21:54:18 +0530

    fix(app.py) Fix minor bugs

--- app.py
+++ app.py
@@ -1,36 +1,14 @@
-from src.model_load import LLMLoad
-from src.prompts import chat_prompt, SYSTEM_PROMPT
-from src.memory import MemoryInit
-# from langchain.chains.llm import LLMChain
+import sys
+from PyQt6.QtWidgets import QApplication
+from ui.controller import ChatUI
 
 
 def main():
-    print("Welcome to the chatbot.")
+    app = QApplication(sys.argv)
+    window = ChatUI()
+    window.show()
+    sys.exit(app.exec())
 
-    # LLM invoke (no control over prompt)
-    llm = LLMLoad()
-
-    # LLM Chain
-    chain = chat_prompt | llm
-
-    # Memory Load + Final conversational chain
-    memory_chain = MemoryInit(chain)
-
-
-        
-    # Loop based CLI
-    while True:
-        user_input = input("Enter the question:")
-        if user_input.lower() == "exit":
-            print("Exiting chat")
-            exit(0)
-
-        try:
-            output = memory_chain.invoke({"user_input": user_input, "system_prompt" : SYSTEM_PROMPT}, config={"configurable": {"session_id": "chat1"}})
-            print(f"Answer: {output.strip()}")
-        except Exception as e:
-            print(f"Error in answering your question {e}")
-        print('-' * 40)
 
 if __name__ == "__main__":
     main()
